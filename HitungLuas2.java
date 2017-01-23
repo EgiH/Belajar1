ec23bb88194691ebeaca3fda56ad086ae6693aba
EgiH <egihermawan91@gmail.com>
2017-01-23 23:27:55 +0700

    perubahan pertama

---
+++ HitungLuas2.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hitungluas;
+
+/**
+ *
+ * @author GIGABYTE
+ */
+public class HitungLuas {
+public static void main (String[]args){
+    
+    int panjang = 5;
+    int lebar = 4;
+    int luas;
+    System.out.println("panjangnya="+panjang);
+    System.out.println("lebarnya="+lebar);
+    luas= (int)2*(panjang*lebar);
+        System.out.println("luasnya adalah="+panjang*lebar);
+}
+   
+}
+ 
